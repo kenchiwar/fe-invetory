4b1d4cfffc08c22f04cecdd84b828254f62411b5
kenchiwar <109209612+kenchiwar@users.noreply.github.com>
2025-03-17 22:47:51 +0700

    add taild win and scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
   const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
@@ -17,7 +14,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
+      <div className="card bg-red-500 text-center">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
