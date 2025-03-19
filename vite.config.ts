afe1031451b8db31a18ecbc8e87e7ec3fc4c0b2e
kenchiwar <109209612+kenchiwar@users.noreply.github.com>
2025-03-19 19:07:19 +0700

    add alias success

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,19 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
+  //setting alias
+  base: "./",
+  build: {
+    outDir: "build",
+  },
   resolve: {
     alias: {
-      "@components": path.resolve(__dirname, "src/components"),
-      "@pages": path.resolve(__dirname, "src/pages"),
-      "@assets": path.resolve(__dirname, "src/assets"),
-      "@styles": path.resolve(__dirname, "src/styles")
-    }
+      "@": path.resolve(__dirname, "./src"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@components": path.resolve(__dirname, "./src/components"),
+    },
   },
+  //end setting alias
   plugins: [react(),
     tailwindcss()
   ]
